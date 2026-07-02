329186f3239e67f5a1a82a84afced492cdaec9a8
myohan_predev <miguelyohan2007@gmail.com>
2026-07-02 20:08:06 -0300

    separacao da transformacao dos dados em funcoes

--- src/transform.py
+++ src/transform.py
@@ -11,16 +11,58 @@ def get_latest_file():
          raise FileNotFoundError("Nenhum arquivo encontrado no diretório data/raw")
     return max(files, key=lambda x: x.stat().st_mtime) # Retorna o maximo em relação ao modification time nos metadados do arquivo
 
-with open(get_latest_file(), 'r', encoding= 'utf-8') as file:
-    data = json.load(file)
 
-# == Tratamento de Nomes / Retirando valores inuteis ==
-# Transformar isso em diferentes funcoes:
-local = pd.Series({"local": data["name"]}, name= "local") # Local no mapa da extração dos dados
-weather_data = pd.Series(data["weather"][0], name= "weather_data").drop(["id", "icon"]).rename({"main": "condition"}) # Pega a condicao de tempo primaria
-temperature_data = pd.Series(data["main"], name= "temperature_data").drop(["pressure", "humidity", "sea_level", "grnd_level"], errors="ignore").rename({"temp": "temperature"})
-wind_data = pd.Series(data["wind"], name= "wind_data").rename({"speed": "wind_speed", "deg": "wind_direction", "gust": "wind_gust_speed"})
-clouds = pd.Series(data["clouds"], name= "clouds_data").rename({"all": "clouds"})
+def local_data_normalize(data: dict) -> pd.Series:
+    return pd.Series({"Local": data["name"]}, name= "local")
 
-all_data = pd.concat(
-    [local, weather_data, temperature_data, wind_data, clouds])
+
+def weather_data_normalize(data: dict) -> pd.Series:
+    weather_data = pd.Series(data["weather"][0], name= "weather_data")
+    weather_data = weather_data.drop(["id", "icon"])
+    weather_data = weather_data.rename({"main": "condition"})
+    return weather_data
+
+
+def temperature_data_normalize(data: dict) -> pd.Series:
+    temperature_data = pd.Series(data["main"], name= "temperature_data")
+    temperature_data = temperature_data.drop(["pressure", "humidity", "sea_level", "grnd_level"], errors="ignore")
+    temperature_data = temperature_data.rename({"temp": "temperature"})
+    return temperature_data
+
+
+def wind_data_normalize(data: dict) -> pd.Series:
+    wind_data = pd.Series(data["wind"], name= "wind_data")
+    wind_data = wind_data.rename({"speed": "wind_speed", "deg": "wind_direction", "gust": "wind_gust_speed"})
+    return wind_data
+
+
+def clouds_data_normalize(data: dict) -> pd.Series: 
+    clouds_data = pd.Series(data["clouds"], name= "clouds_data")
+    clouds_data = clouds_data.rename({"all": "clouds"})
+    return clouds_data
+
+
+def normalize_all(data: dict) -> list[pd.Series]:
+    return [
+        local_data_normalize(data),
+        weather_data_normalize(data),
+        temperature_data_normalize(data),
+        wind_data_normalize(data),
+        clouds_data_normalize(data)
+    ]
+
+
+def concat_all_data(data_normalized: pd.Series) -> pd.DataFrame:
+    all_data = pd.concat(data_normalized)
+    return pd.Series(all_data)
+
+
+def get_data():
+    with open(get_latest_file(), 'r', encoding= 'utf-8') as file:
+        return json.load(file)
+
+
+if __name__ == '__main__':
+    data = get_data()
+    data_normalized = normalize_all(data)
+    print(concat_all_data(data_normalized))
